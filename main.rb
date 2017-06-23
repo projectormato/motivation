1bcb6fede08ccd6121fbb8da05553a881397f2f3
projectormato <k.keita811.anuy@gmail.com>
2017-06-24 00:05:59 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'line/bot'
+
+# 微小変更部分！確認用。
+get '/' do
+  "Hello world"
+end
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LineMessageAPIChannelSecret"]
+    config.channel_token = ENV["LineMessageAPIChannelAccessToken"]
+  }
+end
+
+post '/callback' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open("content")
+        tf.write(response.body)
+      end
+    end
+  }
+
+  "OK"
+end
