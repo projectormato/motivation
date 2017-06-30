7da5e25608243e80dfd36a2f7cc4f570208df551
projectormato <k.keita811.anuy@gmail.com>
2017-06-30 14:19:41 +0900

    画像の場合処理を変えてみる

--- main.rb
+++ main.rb
@@ -31,12 +31,18 @@ post '/callback' do
           type: 'text',
           text: 'こんにちは'
         }
-        puts message
+        puts message #message出せるかな
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
         tf.write(response.body)
+        message = {
+          type: 'text',
+          text: 'これは画像ですね'
+        }
+        puts message #message出せるかな
+        client.reply_message(event['replyToken'], message)
       end
     end
   }
